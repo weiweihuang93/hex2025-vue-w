79a45aefc8f4ff7c57b3e817316fc3f130ea93fd
weiweihuang93 <weiwei.huang.93@gmail.com>
2025-08-09 08:05:42 +0800

    feat(router): 新增 Vue Router，加入初始路由

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container py-2">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="container mt-4">
+    <div>
+      <h1 class="fs-3 mb-4">歡迎來到 Vue 任務總覽</h1>
+    </div>
+  </div>
+</template>
